8fce68d43ffda4c692efe0df8b2ec70da3f64129
Marisa Wyatt <risawyatt22@gmail.com>
2023-03-01 16:13:34 -0700

    Add 3 different methods to edit arrays and explinations

--- arrays.js
+++ arrays.js
@@ -2,4 +2,14 @@ var fastFood = ["Mc Donald's", "Chick-fil-a", "Taco Bell", "Wendy's"];
 
 var ages = [12, 23, 34, 42, 17];
 
-var quizAnswers = [true, false, false, true, false]
+var quizAnswers = [true, false, false, true, false];
+
+fastFood.pop();
+console.log(fastFood);
+// Pop removes the last element in an array, in this example, "Wendy's" will be removed.
+ages.push(56);
+console.log(ages);
+// Push adds an element to the last index position in an array, in this example the age 56 will be added to the 5th index position
+quizAnswers.unshift(true);
+console.log(quizAnswers);
+// Unshift adds an element to the beginning of an array in the 0 index postions and shifts all other positions to the right. This example now has the new boolean var as postion 0 and the array ends in position 5 instead of 4.
